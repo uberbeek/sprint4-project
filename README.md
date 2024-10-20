441effe951b8ed5b8d90b0d3a91a96573aa3796d
uberbeek <uberbeek@gmail.com>
2024-10-20 03:20:27 -0700

    Initial commit of app.py and related files

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # sprint4-project
 This is my project for TripleTen Sprint 4: Software Development Tools 
+
+The purpose of this project is to demonstrate that I can write and deploy a simple web app.
+
+The app displays vehicle data, plots several charts, and features an interactive element.
+
+Plots:
+- Bar chart of manufacturer by condition
+- Bar chart of vehicle type by paint color
+- Histogram of odometer by type (with a checkbox for outlyers > 300,000 miles)
+- Scatterplot of price by model year (with a checkbox for model years since 1960)
+- Histograms for designs of user selected manufacturer and type (with a checkbox to normalize)
+
+Libraries used:
+- pandas
+- plotly.express
+- streamlit
+
+Dataset:
+- vehicles_us.csv (https://practicum-content.s3.us-west-1.amazonaws.com/datasets/vehicles_us.csv)
+
+How to Run Locally:
+- Clone the repository: git clone https://github.com/uberbeek/sprint4-project
+- Install the required Python packages: pip install -r requirements.txt
+- Run the Streamlit app: streamlit run app.py
+- The app should now be available at http://localhost:8501
---
+++ app.py
@@ -0,0 +1,93 @@
+# Import packages
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+
+# Import data and extract manufacturer from model column
+df = pd.read_csv('vehicles_us.csv')
+df['manufacturer'] = df['model'].str.split().str[0]
+df['design'] = df['model'].str.split().str[1:].str.join(' ')
+
+
+# Display the dataframe
+st.header('Vehicle Data')
+st.dataframe(df)
+
+
+# Display bar chart of manufacturer by condition
+st.header('Manufacturer by Condition')
+st.write(px.histogram(df, x='manufacturer', color='condition'))
+
+
+# Display bar chart of vehicly type by paint color
+st.header('Vehicle Type by Paint Color')
+category_order = {
+    "paint_color": ["custom", "black", "grey", "silver", "white", "brown", "purple", "blue", "green", "yellow", "orange", "red"]
+}
+color_map = {
+    "red": "red",
+    "orange": "orange",
+    "yellow": "yellow",
+    "green": "green",
+    "blue": "blue",
+    "purple": "purple",
+    "brown": "brown",
+    "white": "#f0f0f0",
+    "silver": "silver",
+    "grey": "grey",
+    "black": "black",
+    "custom": "pink"
+}
+st.write(px.histogram(df, x='type', color='paint_color',
+         color_discrete_map=color_map, category_orders=category_order))
+
+
+# Display histogram of odometer by type with a checkbox for outlyers > 300,000 miles
+st.header('Histogram of Odometer by Type')
+
+show_outliers = st.checkbox('Include outliers with > 300,000 miles')
+df_filtered_by_odometer = df if show_outliers else df[df['odometer'] < 300000]
+
+st.write(px.histogram(df_filtered_by_odometer, x='odometer', color='type'))
+
+
+# Display scatterplot of price by model year with a checkbox for model years since 1960
+st.header('Scatter Plot of Price by Model Year')
+
+show_before_1960 = st.checkbox('Include vehicles from before 1960')
+df_filtered_by_year = df if show_before_1960 else df[df['model_year'] >= 1960]
+
+st.write(px.scatter(df_filtered_by_year,
+         x='model_year', y='price'))
+
+
+# Display histograms for designs of the selected manufacturer and type
+st.header(
+    'Compare the Price Distribution for Designs of Selected Manufacturer and Type')
+
+manufac_list = sorted(df['manufacturer'].unique())
+manufacturer_selected = st.selectbox('Select Manufacturer',
+                                     manufac_list, index=manufac_list.index('chevrolet'))
+
+eligible_types = sorted(
+    df[df['manufacturer'] == manufacturer_selected]['type'].unique())
+if 'type_selected' not in st.session_state or st.session_state['type_selected'] not in eligible_types:
+    st.session_state['type_selected'] = eligible_types[0]
+type_selected = st.selectbox('Select Vehicle Type', eligible_types,
+                             index=eligible_types.index(st.session_state['type_selected']))
+st.session_state['type_selected'] = type_selected
+
+mask_filter = (df['manufacturer'] == manufacturer_selected) & (
+    df['type'] == type_selected)
+df_filtered_by_make_type = df[mask_filter]
+
+normalize = st.checkbox('Normalize histogram', value=True)
+histnorm = 'percent' if normalize else None
+
+st.write(px.histogram(df_filtered_by_make_type,
+                      x='price',
+                      nbins=30,
+                      color='design',
+                      histnorm=histnorm,
+                      barmode='overlay'))
